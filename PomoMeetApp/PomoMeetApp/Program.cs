710827b6a9afbc5f49f5e4f391eec4969e0c485f
Linh-Phuong-Peony <linhphuongtranltv@gmail.com>
2025-03-28 22:38:54 +0700

    Create Sign Up UI

--- PomoMeetApp/PomoMeetApp/Program.cs
+++ PomoMeetApp/PomoMeetApp/Program.cs
@@ -1,3 +1,5 @@
+using PomoMeetApp.View;
+
 namespace PomoMeetApp
 {
     internal static class Program
@@ -11,7 +13,8 @@ namespace PomoMeetApp
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new StartApp());
+
         }
     }
 }
