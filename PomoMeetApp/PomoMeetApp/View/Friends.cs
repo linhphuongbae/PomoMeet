a43294f89c1ac18176238c71364a887d92d43ea8
faviconico1910 <23521179@gm.uit.edu.vn>
2025-05-04 17:54:32 +0700

    overview of FriendRequests

--- PomoMeetApp/PomoMeetApp/View/Friends.cs
+++ PomoMeetApp/PomoMeetApp/View/Friends.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Google.Cloud.Firestore;
 using PomoMeetApp.Classes;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace PomoMeetApp.View
 {
@@ -33,10 +34,115 @@ namespace PomoMeetApp.View
             pnFriends.Visible = true;
         }
 
-        private void btn_FriendRequests_Click(object sender, EventArgs e)
+        private async void btn_FriendRequests_Click(object sender, EventArgs e)
         {
             lbAllFriends.Text = "Friends Requests";
             pnFriends.Visible = true;
+
+            // logic view friendrequests
+            pnFriends.Controls.Clear();
+            string curUserId = UserSession.CurrentUser.UserId;
+            var db = FirebaseConfig.database;
+            // lấy receiver_id == current user id
+            var request = await db.Collection("FriendShips").WhereEqualTo("receiver_id", curUserId).GetSnapshotAsync();
+
+            foreach ( var item in request.Documents)
+            {
+                string requesterId = item.GetValue<string>("requester_id");
+
+                // Lấy thông tin người gửi từ bảng User
+                var userSnap = await db.Collection("User").Document(requesterId).GetSnapshotAsync();
+                if (!userSnap.Exists) continue;
+
+                string fromUsername = userSnap.GetValue<string>("Username");
+                string avatarId = userSnap.ContainsField("Avatar") ? userSnap.GetValue<string>("Avatar") : "avt1"; // fallback
+
+
+                // Load ảnh avatar từ Firebase Storage nếu có, hoặc từ local nếu là mã như "avt1"
+                Image avatarImage = LoadLocalAvatar(avatarId);
+
+                // Panel chứa avatar và tên
+                Panel card = new Panel
+                {
+                    Width = 200,
+                    Height = 400,
+                    BackColor = Color.Transparent,
+                    BorderStyle = BorderStyle.None,
+                    Margin = new Padding(5)
+                };
+                // Tạo SiticoneCirclePictureBox
+                PictureBox pic = new PictureBox
+                {
+                    Width = 100,
+                    Height = 100,
+                    Location = new Point((card.Width - 100) / 2, 10),
+                    Image = avatarImage,
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                    BackColor = Color.Transparent,
+                };
+
+
+                // Tạo Label tên người dùng
+                Label lbl = new Label
+                {
+                    Text = fromUsername,
+                    Location = new Point((card.Width - TextRenderer.MeasureText(fromUsername, new Font("Segoe UI", 10, FontStyle.Bold)).Width) / 2, 120),
+                    AutoSize = true,
+                    Font = new Font("Segoe UI", 10, FontStyle.Bold)
+                };
+                var btnAccept = new SiticoneNetCoreUI.SiticoneButton
+                {
+                    Text = "Accept",
+
+                    // thiết kế
+                    Width = 100,
+                    Height = 30,
+                    Location = new Point((card.Width - 100) / 2, 150),
+                    ForeColor = Color.Black,
+                    GlowColor = Color.FromArgb(117, 164, 127),
+                    CornerRadiusBottomLeft = 10,
+                    CornerRadiusBottomRight = 10,
+                    CornerRadiusTopLeft = 10,
+                    CornerRadiusTopRight = 10,
+                    BorderWidth = 2,
+                    ButtonBackColor = Color.White,
+                    HoverBackColor = Color.FromArgb(252, 255, 224),
+                    GradientColor = Color.FromArgb(117, 164, 127),
+                    BorderColor = Color.FromArgb(117, 164, 127),
+                    TextColor = Color.FromArgb(117, 164, 127),
+                    Font = new Font("Segoe UI", 9, FontStyle.Bold)
+                };
+                var btnDecline = new SiticoneNetCoreUI.SiticoneButton
+                {
+                    Text = "Decline",
+
+                    // thiết kế
+                    Width = 100,
+                    Height = 30,
+                    Location = new Point((card.Width - 100) / 2, 185),
+                    ForeColor = Color.Black,
+                    GlowColor = Color.FromArgb(117, 164, 127),
+                    CornerRadiusBottomLeft = 10,
+                    CornerRadiusBottomRight = 10,
+                    CornerRadiusTopLeft = 10,
+                    CornerRadiusTopRight = 10,
+                    BorderWidth = 2,
+                    ButtonBackColor = Color.White,
+                    HoverBackColor = Color.FromArgb(252, 255, 224),
+                    GradientColor = Color.FromArgb(117, 164, 127),
+                    BorderColor = Color.FromArgb(240, 128, 128),
+                    TextColor = Color.FromArgb(240, 128, 128),
+                    Font = new Font("Segoe UI", 9, FontStyle.Bold)
+                };
+                card.Controls.Add(pic);
+                card.Controls.Add(lbl);
+                card.Controls.Add(btnAccept);
+                card.Controls.Add(btnDecline);
+
+                // Thêm vào pnFindFriends
+                pnFriends.Controls.Add(card);
+            } 
+                
         }
 
         private async void btn_FindFriends_Click(object sender, EventArgs e)
