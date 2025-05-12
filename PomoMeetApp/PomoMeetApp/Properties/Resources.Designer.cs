4e4d6381fcf2cd5e7ea6b21c1aa9ed4231e10fc3
Linh-Phuong-Peony <linhphuongtranltv@gmail.com>
2025-05-12 22:12:50 +0700

    fix search button (Room Request Form)

--- PomoMeetApp/PomoMeetApp/Properties/Resources.Designer.cs
+++ PomoMeetApp/PomoMeetApp/Properties/Resources.Designer.cs
@@ -793,6 +793,16 @@ namespace PomoMeetApp.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Search_2 {
+            get {
+                object obj = ResourceManager.GetObject("Search_2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Send {
             get {
                 object obj = ResourceManager.GetObject("Send", resourceCulture);
